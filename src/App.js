6f5c46e5c5f7ffad26250cd292f60731ef909fe4
gitpeut <peut@peut.org>
2021-07-31 01:13:13 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    const [state, toggleState] = React.useState({ lighton: true });
+    const [state, toggleState] = React.useState({lighton: true});
 
-    function toggleClick(e){
+    function toggleClick(e) {
 
         let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
-        if ( typeof state[e.target.name] === "boolean"  ) {
+        if (typeof state[e.target.name] === "boolean") {
+            //toggle boolean
             value = !state[e.target.name];
         }
-        toggleState( {...state, [e.target.name]: value } );
+        toggleState({...state, [e.target.name]: value});
+
     }
 
     return (
-    <div>
-        <button
-            type="button"
-            name="lighton"
-            onClick={ toggleClick }
-        >
-        {state.lighton ? 'AAN' : 'UIT'}
-        </button>
-    </div>
+        <div>
+            <button
+                type="button"
+                name="lighton"
+                onClick={toggleClick}
+            >
+                {state.lighton ? 'AAN' : 'UIT'}
+            </button>
+        </div>
     );
 }
 
